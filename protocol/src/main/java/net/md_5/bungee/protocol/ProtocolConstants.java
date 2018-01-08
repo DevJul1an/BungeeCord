05dcd97b8badbb0d6a05506688c5f9d59d323644
Julian <jul1an_k@elitepvp.ml>
2018-01-08 16:34:07 +0100

    Make ViaVersion working here.

--- protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
@@ -27,10 +27,10 @@ public class ProtocolConstants
             "1.12.x"
     );
 
-    public static final List<Integer> SUPPORTED_VERSION_IDS = Arrays.asList( 
+    public static final List<Integer> SUPPORTED_VERSION_IDS = Arrays.asList(
+            ProtocolConstants.MINECRAFT_1_8,
             ProtocolConstants.MINECRAFT_1_7_2,
             ProtocolConstants.MINECRAFT_1_7_6,
-            ProtocolConstants.MINECRAFT_1_8,
             ProtocolConstants.MINECRAFT_1_9,
             ProtocolConstants.MINECRAFT_1_9_1,
             ProtocolConstants.MINECRAFT_1_9_2,
