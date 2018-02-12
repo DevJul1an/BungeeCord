11e62d535cfb4beccfac3441a21b6d24ed8c550d
Julian <jul1an_k@minezone.biz>
2018-02-12 18:49:11 +0100

    Debug to fix something

--- proxy/src/main/java/net/md_5/bungee/ServerConnection.java
+++ proxy/src/main/java/net/md_5/bungee/ServerConnection.java
@@ -41,6 +41,8 @@ public class ServerConnection implements Server
     @Override
     public void sendData(String channel, byte[] data)
     {
+        System.out.println("Channel: " + channel);
+    
         unsafe().sendPacket( new PluginMessage( channel, data, forgeServer ) );
     }
 
