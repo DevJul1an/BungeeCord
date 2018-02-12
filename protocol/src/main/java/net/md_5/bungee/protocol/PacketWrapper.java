c7b7b314f6a53d013f7ffd01ccc4d2d86687891c
Julian <jul1an_k@minezone.biz>
2018-02-12 20:04:54 +0100

    Update PacketWrapper.java

--- protocol/src/main/java/net/md_5/bungee/protocol/PacketWrapper.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/PacketWrapper.java
@@ -3,12 +3,13 @@ package net.md_5.bungee.protocol;
 import io.netty.buffer.ByteBuf;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.Getter;
 
 @RequiredArgsConstructor
 public class PacketWrapper
 {
 
-    public final DefinedPacket packet;
+    @Getter public final DefinedPacket packet;
     public final ByteBuf buf;
     @Setter
     private boolean released;
